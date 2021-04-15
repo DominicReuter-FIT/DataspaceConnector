908c1269a67166af7d676c49ce834afa0b6969eb
Dominic Reuter <dominic.reuter@fit.fraunhofer.de>
2021-04-15 14:09:24 +0200

    feat: Webhook Repository Integration

---
+++ src/main/java/de/fraunhofer/isst/dataspaceconnector/repositories/WebhookEventRepository.java
@@ -0,0 +1,6 @@
+package de.fraunhofer.isst.dataspaceconnector.repositories;
+
+import de.fraunhofer.isst.dataspaceconnector.model.webhook.harbor.Event;
+
+public interface WebhookEventRepository extends BaseEntityRepository<Event> {
+}
